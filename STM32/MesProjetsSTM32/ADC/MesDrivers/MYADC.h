013dd2504fe7929b0aff52d009e2171ad65c6d17
Oussama <eljidoussama155@gmail.com>
2023-11-26 18:46:17 +0100

    ADC driver hope that's gd

--- STM32/MesProjetsSTM32/ADC/MesDrivers/MYADC.h
+++ STM32/MesProjetsSTM32/ADC/MesDrivers/MYADC.h
@@ -4,4 +4,15 @@
 #include "stm32f10x.h"
 #include "MyGPIO.h"
 
-void start_ADC(ADC_TypeDef*ADC , char nbconver , char voie);
+/**
+ * @brief Initialise l'ADC donn? sur le channel donn? en mode Single ou (nbconver) Conversion.
+ * Le GPIO correspondant au channel donn? doit ?tre configur? en Output Push Pull
+ * que cela fonctionne.
+ * @example 	Port_IO_Init_Input(GPIOA, 1);
+* 					  Init_ADC_Single_Conv(ADC1, 1);
+ */
+void start_ADC(ADC_TypeDef * ADC , char nbconver , char voie);
+
+
+/*recuperation des valeurs convertis*/
+uint16_t ADC_Value(ADC_TypeDef * ADC);
